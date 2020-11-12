7fc74f2601c0be1b34575c17e7950c988e20104c
chaukuo <70113910+chaukuo@users.noreply.github.com>
2020-11-12 18:39:08 -0300

    Update Covid_WHO_Indicators.R

--- Covid_WHO_Indicators.R
+++ Covid_WHO_Indicators.R
@@ -186,17 +186,26 @@ kmedio <- kmeans(dados_df, centers = 5)
 
 kmedio
 
-tibble(coord1 = kmedio$Handwashing, 
-       coord2 = kmedio$Hospital.beds, 
-       label = rownames(dados_df), 
-       cluster = factor(cutree(kmedio, 5))) %>% 
-  ggplot(aes(coord1, coord2)) + 
-  geom_text_repel(aes(label = label), size = 3) +
-  geom_point(aes(color = cluster), size = 3, show.legend = FALSE) + 
-  theme_bw()
+auxiliar <- tibble(cluster = kmedio$cluster) %>% 
+  bind_cols(as_tibble(dados_df))
 
-teste$withinss # para acessar a variável
-# W(Ck)=∑xi∈Ck(xi−μk)2
+# Total SS
+
+kmedio$totss
+
+auxiliar %>% 
+  transmute(Handwashing = (Handwashing - mean(Handwashing))^2, 
+            Hospital.beds = (Hospital.beds - mean(Hospital.beds))^2,
+            Public.medicine.availability = (Public.medicine.availability - mean(Public.medicine.availability))^2,
+            Health.Legislation = (Health.Legislation - mean(Health.Legislation))^2,
+            Health.Coordination = (Health.Coordination - mean(Health.Coordination))^2,
+            Health.expense.GDP = (Health.expense.GDP - mean(Health.expense.GDP))^2,
+            totss = Handwashing + Hospital.beds + Public.medicine.availability + Health.Legislation + Health.Coordination + Health.expense.GDP) %>% 
+  summarise(totss = sum(totss))
+
+# WITHIN SS
+
+kmedio$withinss # para acessar a variável
 
 auxiliar %>% 
   group_by(cluster) %>% 
@@ -206,5 +215,56 @@ auxiliar %>%
             Health.Legislation = sum((Health.Legislation - mean(Health.Legislation))^2),
             Health.Coordination = sum((Health.Coordination - mean(Health.Coordination))^2),
             Health.expense.GDP = sum((Health.expense.GDP - mean(Health.expense.GDP))^2),
-            total_litres_alcohol = sum((total_litres_alcohol - mean(total_litres_alcohol))^2)) %>% 
-  mutate(within = beer + spirit + wine + total_litres_alcohol)
+            totss = Handwashing + Hospital.beds + Public.medicine.availability + Health.Legislation + Health.Coordination + Health.expense.GDP) %>%
+  mutate(within = Handwashing + Hospital.beds + Public.medicine.availability + Health.Legislation + Health.Coordination + Health.expense.GDP)
+
+# método do cotovelo ------------------------------------------------------
+
+criterio <- function(k) kmeans(dados_df, k)$tot.withinss
+
+estudo <- tibble(k = 1:15) %>% 
+  mutate(w = map_dbl(k, criterio))
+
+estudo %>% 
+  ggplot(aes(k, w)) + 
+  geom_point(size = 3) + 
+  geom_line() + 
+  labs(y = "total within sum of squares", x = "k") +
+  scale_x_continuous(breaks = 1:15)
+
+# especifica k  
+
+kmedias <- kmeans(dados_df, centers = 6)
+
+dados <- dados %>% 
+  mutate(cluster = kmedias$cluster)
+
+dados %>% 
+  mutate(cluster = as.factor(cluster)) %>% 
+  ggplot(aes(Handwashing, Hospital.beds, color = cluster)) + 
+  geom_point(size = 3, alpha = .5) + 
+  theme(legend.position = "top")
+
+# silhoutte ---------------------------------------------------------------
+
+kmedias <- kmeans(dados_df, centers = 6)
+
+res_silhouette <- silhouette(kmedias$cluster, dist(dados_df)^2)
+
+res_silhouette
+
+fviz_silhouette(res_silhouette) 
+
+fviz_nbclust(dados_df, kmeans, method = "silhouette")
+
+# gap ---------------------------------------------------------------------
+
+res_gap <- clusGap(dados_df, 
+                   FUN = kmeans, 
+                   d.power = 2,
+                   nstart = 30, 
+                   iter.max = 30,
+                   K.max = 20, 
+                   B = 50)
+
+fviz_gap_stat(res_gap)
